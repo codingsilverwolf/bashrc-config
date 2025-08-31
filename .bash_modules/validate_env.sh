9a9b4017b44f0d0f7e41ec708d919ca172c622c7
codingsilverwolf <17933209+codingsilverwolf@users.noreply.github.com>
2025-08-31 20:12:32 -0300

    volver a versión sin return 1

--- .bash_modules/validate_env.sh
+++ .bash_modules/validate_env.sh
@@ -1,5 +1,5 @@
-validate_env() {
-  [ -z "$ENV_TYPE" ] && echo "❌ ENV_TYPE no definido. Abortando módulo: $(basename "${BASH_SOURCE[1]}")" && return 1
-  [ -z "$DOCS_PATH" ] && echo "❌ DOCS_PATH no definido. Abortando módulo: $(basename "${BASH_SOURCE[1]}")" && return 1
-  [ -z "$SSH_KEY_PATH" ] && echo "❌ SSH_KEY_PATH no definido. Abortando módulo: $(basename "${BASH_SOURCE[1]}")" && return 1
-}
+# ── Módulo: validate_env ──
+
+[ -z "$ENV_TYPE" ] && echo "❌ ENV_TYPE no definido. Abortando módulo: $(basename "${BASH_SOURCE[1]}")" && return
+[ -z "$DOCS_PATH" ] && echo "❌ DOCS_PATH no definido. Abortando módulo: $(basename "${BASH_SOURCE[1]}")" && return
+[ -z "$SSH_KEY_PATH" ] && echo "❌ SSH_KEY_PATH no definido. Abortando módulo: $(basename "${BASH_SOURCE[1]}")" && return
